4723993f97540c22c60c02b6931973291875d674
Mouse <mouse0w0@qq.com>
2022-06-01 00:19:40 +0800

    version 25: application

--- src/main/java/com/github/mouse0w0/mike/Mike.java
+++ src/main/java/com/github/mouse0w0/mike/Mike.java
@@ -1,9 +1,27 @@
 package com.github.mouse0w0.mike;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Mike {
     public static void main(String[] args) {
-        Generator.generate(new Project(Paths.get("D:\\Users\\Mouse\\Desktop\\毕业设计\\cmake-demo\\Demo3")));
+        Path dir;
+        if (args.length == 0) {
+            dir = Paths.get(System.getProperty("user.dir"));
+        } else if (args.length == 1) {
+            dir = Paths.get(args[0]).toAbsolutePath();
+        } else {
+            System.out.println("Too many arguments, usage 'mike [folder]'");
+            return;
+        }
+        System.out.println("Generating Makefile at " + dir);
+        Path mikefile = dir.resolve(Project.MIKEFILE);
+        if (Files.notExists(mikefile)) {
+            System.out.println("No found " + mikefile + ", stop generating");
+            return;
+        }
+        Generator.generate(new Project(dir));
+        System.out.println("Generating Makefile successfully");
     }
 }
